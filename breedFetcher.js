36fee8fff790ceb837207283e834cb673babfcb0
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-03-03 22:42:40 -0500

    breedFetcher function implemented

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,33 @@
 const request = require('request'); 
 
+const breedFetcher = (breed) =>  {
+  const apiurl = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+request.get(apiurl, (error, response, body) => {
+if (error) {
+  console.log("Error with making request");
+  return;
+}
+
+const breeds = JSON.parse(body);
+
+if (breeds.length === 0) {
+  console.log(`Breed "${breed}" not found.`);
+  return;
+}
+
+const firstBreed = breeds[0];
+      console.log(`Breed: ${firstBreed.name}`);
+      console.log(`Description: ${firstBreed.description}`);
+    }
+
+)};
+
+
+const [, , userBreed] = process.argv;
+
+if (!userBreed) {
+  console.error('Usage: node breedFetcher.js <breed-name>');
+} else {
+  breedFetcher(userBreed);
+}
